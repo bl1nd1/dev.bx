82d0ad84f90b21306d7c0feab3caf8bc968045d8
PopovAlex <c76167970@gmail.com>
2021-11-10 00:12:40 +0200

    bitflix.php: deleted extra ifs searchbar.css: fixed searchbar

--- bitflix/bitflix.php
+++ bitflix/bitflix.php
@@ -9,58 +9,21 @@ require_once "./config/cfg.php";
 require_once "./data/movies.php";
 require_once "./lib/template-functions.php";
 
-$currentPage = "./res/pages/main.php";
 $currentMenuItem = $_GET['menuItem'] ?? 'main';
 $genre = array_key_exists($currentMenuItem, $genres) ? $genres[$currentMenuItem] : '';
-$isFavorite = $_GET['favorite'];
-$addMovie = isset($_GET['addMovie']);
-
 $request = $_GET['request'] ?? "";
 
-if (isset($_GET['id']))
+$filteredMovie = filterMoviesByGenre($movies, $genre);
+$filteredMovie = filterMoviesByUserRequest($filteredMovie, $request);
+if (!empty($filteredMovie))
 {
-	$id = (int)$_GET['id'];
-	$currentMovie = findMovieByID($movies, $id);
-
-	if ($currentMovie)
-	{
-		$currentPage = "./res/pages/detailed-film.php";
-		$result = renderTemplate($currentPage, [
-			'movie' => $currentMovie
-		]);
-	}
-	else
-	{
-		$currentPage = "./res/pages/movie404.php";
-		$result = renderTemplate($currentPage);
-	}
-}
-elseif ($currentMenuItem === $config['menu'][array_key_last($config['menu'])]['name'])
-{
-	$currentPage = "./res/pages/favorite.php";
-	$result = renderTemplate($currentPage);
-}
-elseif ($addMovie)
-{
-	$currentPage = "./res/pages/addMovie.php";
-	$result = renderTemplate($currentPage);
+	$result = renderTemplate("./res/pages/main.php", [
+		'movies' => $filteredMovie,
+	]);
 }
 else
 {
-	$filteredMovie = filterMoviesByGenre($movies, $genre);
-	$filteredMovie = filterMoviesByUserRequest($filteredMovie, $request);
-	if (!empty($filteredMovie))
-	{
-		$currentPage = "./res/pages/main.php";
-		$result = renderTemplate($currentPage, [
-			'movies' => $filteredMovie,
-		]);
-	}
-	else
-	{
-		$currentPage = "./res/pages/movie404.php";
-		$result = renderTemplate($currentPage);
-	}
+	$result = renderTemplate("./res/pages/movie404.php");
 }
 
 renderLayout($result, [
