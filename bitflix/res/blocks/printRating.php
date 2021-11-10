f71f04b7fb79e802556cf4ad0798d6f7f522bfe2
PopovAlex <c76167970@gmail.com>
2021-11-10 17:04:13 +0200

    bitflix.php: deleted extra ifs searchbar.css: fixed searchbar

--- bitflix/res/blocks/printRating.php
+++ bitflix/res/blocks/printRating.php
@@ -6,11 +6,11 @@
 <div class="detailed-movie-item--body-info-rating">
 	<?php
 	for ($i = 0; $i < 10; $i++): ?>
-		<div class="info-rating-rectangle<?= (round($movie['rating']) > $i)? '-active' : '' ?>"></div>
+		<div class="detailed-movie-item--body-info-rating-list-item<?= (round($movie['rating']) > $i)? '-active' : '' ?>"></div>
 	<?php
 	endfor; ?>
-	<div class="info-rating-circle">
-		<div class="circle-number">
+	<div class="detailed-movie-item--body-info-rating-circle">
+		<div class="detailed-movie-item--body-info-rating-circle-number">
 			<?= formatRating($movie['rating']) ?>
 		</div>
 	</div>
